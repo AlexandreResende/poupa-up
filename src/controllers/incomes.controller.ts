ed8334b60f3d6d148d7d67b14e33015ca13227cb
Alexandre Resende <aar6947@gmail.com>
2018-10-10 07:55:00 -0300

    chore: Implement update and delete controllers

--- src/controllers/incomes.controller.ts
+++ src/controllers/incomes.controller.ts
@@ -1,7 +1,7 @@
 
 import {Request, Response} from 'express';
 import { IncomeService } from '../models-persistence/incomes.model';
-import { createIncomeOutput } from '../data-source/incomes.ds';
+import {createIncomeOutput } from '../data-source/incomes.ds';
 
 export const getAllIncomes = (req: Request, res:Response) => {
   res.send('getallIncomes');
@@ -18,10 +18,16 @@ export const createIncome = async (req: Request, res:Response) => {
   res.status(200).send(createIncomeResponse);
 }
 
-export const updateIncome = (req: Request, res:Response) => {
-  res.send('updateincomes');
+export const updateIncome = async (req: Request, res:Response) => {
+  const { id, ...rest } = req.body;
+  const updateIncomeRespose = await new IncomeService().updateIncome(id, rest);
+
+  res.status(200).send(updateIncomeRespose);
 }
 
-export const deleteIncome = (req: Request, res:Response) => {
-  res.send('deleteincome');
+export const deleteIncome = async (req: Request, res:Response) => {
+  const { id } = req.body;
+  const deleteIncomeResponse = await new IncomeService().deleteIncome(id);
+
+  res.status(200).send(deleteIncomeResponse);
 }
