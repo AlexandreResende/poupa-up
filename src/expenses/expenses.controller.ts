2d1f55de006a4732798c79b1f14145070c285ce4
Alexandre Resende <aar6947@gmail.com>
2019-01-01 22:06:54 -0200

    refactor: Handle database calls from controller with a catch statement

--- src/expenses/expenses.controller.ts
+++ src/expenses/expenses.controller.ts
@@ -4,21 +4,30 @@ import { ExpenseService } from './expenses.model';
 import { ICreateExpenseOutput } from './expenses.ds';
 
 export const getAllExpenses = async (req: Request, res: Response) => {
-  const getAllExpenseResponse = await new ExpenseService().getAllExpenses();
+  const getAllExpenseResponse = await new ExpenseService().getAllExpenses()
+    .catch(err => {
+      throw new Error(`Could not retrieve all expenses ${err}`);
+    });
 
   res.status(200).send(getAllExpenseResponse);
 }
 
 export const getMonthlyExpense = async (req: Request, res: Response) => {
   const { month, year } = req.body;
-  const getMonthlyExpenseResponse = await new ExpenseService().getMonthlyExpenses(month, year);
+  const getMonthlyExpenseResponse = await new ExpenseService().getMonthlyExpenses(month, year)
+    .catch(err => {
+      throw new Error(`Could not retrieve monthly expenses ${err}`);
+    });
 
   res.status(200).send(getMonthlyExpenseResponse);
 }
 
 export const createExpense = async (req: Request, res: Response) => {
   const createExpenseReq = req.body;
-  const createExpenseResponse: ICreateExpenseOutput = await new ExpenseService().createExpense(createExpenseReq);
+  const createExpenseResponse: ICreateExpenseOutput = await new ExpenseService().createExpense(createExpenseReq)
+    .catch(err => {
+      throw new Error(`Could not create expense ${err}`);
+    });
   
   res.status(200).send(createExpenseResponse);
 }
@@ -27,7 +36,7 @@ export const updateExpense = async (req: Request, res: Response) => {
   const { id, ...rest } = req.body;
   const updateExpenseRespose = await new ExpenseService().updateExpense(id, rest)
     .catch(err => {
-      throw new Error('Could not update Expense.');
+      throw new Error(`Could not update Expense ${err}`);
     });
 
   res.status(200).send(updateExpenseRespose);
@@ -35,7 +44,10 @@ export const updateExpense = async (req: Request, res: Response) => {
 
 export const deleteExpense = async (req: Request, res: Response) => {
   const { id } = req.body;
-  const deleteExpenseResponse = await new ExpenseService().deleteExpense(id);
+  const deleteExpenseResponse = await new ExpenseService().deleteExpense(id)
+    .catch(err => {
+      throw new Error(`Could not delete expense ${err}`);
+    });
 
   res.status(200).send(deleteExpenseResponse);
 }
