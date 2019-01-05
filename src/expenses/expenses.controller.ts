caf1d9ceb209d72db0d17032a9792f3d30f7d2a6
Alexandre Resende <aar6947@gmail.com>
2019-01-05 20:42:15 -0200

    fix: Adjust controllers to work with a try/catch structure

--- src/expenses/expenses.controller.ts
+++ src/expenses/expenses.controller.ts
@@ -1,88 +1,91 @@
 
-import { ICreateExpenseOutput } from "./expenses.ds";
-
 import {Request, Response} from "express";
 import { ExpenseService } from "./expenses.model";
 
 export const getAllExpenses = async (req: Request, res: Response) => {
-  const { expenses } = await new ExpenseService().getAllExpenses()
-    .catch((err) => {
-      res.status(500).json({
-        error: `Could not retrieve all expenses ${err}`,
-        expenses: null,
-      });
-    });
+  try {
+    const { expenses } = await new ExpenseService().getAllExpenses();
 
-  res.status(200).json({
-    error: null,
-    expenses,
-  });
+    res.status(200).json({
+      error: null,
+      expenses,
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: `Could not retrieve all expenses ${err}`,
+      expenses: null,
+    });
+  }
 };
 
 export const getMonthlyExpense = async (req: Request, res: Response) => {
   const { month, year } = req.params;
-  const { expenses } = await new ExpenseService().getMonthlyExpenses(month, year)
-    .catch((err) => {
-      res.status(500).json({
-        error: `Could not retrieve monthly expenses ${err}`,
-        expenses: null,
-      });
-    });
+  try {
+    const { expenses } = await new ExpenseService().getMonthlyExpenses(month, year);
 
-  res.status(200).json({
-    error: null,
-    expenses,
-  });
+    res.status(200).json({
+      error: null,
+      expenses,
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: `Could not retrieve monthly expenses ${err}`,
+      expenses: null,
+    });
+  }
 };
 
 export const createExpense = async (req: Request, res: Response) => {
   const createExpenseReq = req.body;
-  const createExpenseResponse = await new ExpenseService().createExpense(createExpenseReq)
-    .catch((err) => {
-      res.status(500).json({
-        error: `Could not create expense ${err}`,
-        expenses: null,
-      });
-    });
+  try {
+    const createExpenseResponse = await new ExpenseService().createExpense(createExpenseReq);
 
-  res.status(200).json({
-    error: null,
-    expenses: createExpenseResponse,
-  });
+    res.status(200).json({
+      error: null,
+      expenses: createExpenseResponse,
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: `Could not create expense ${err}`,
+      expenses: null,
+    });
+  }
 };
 
 export const updateExpense = async (req: Request, res: Response) => {
   const { id, ...rest } = req.body;
-  const updateExpenseRespose = await new ExpenseService().updateExpense(id, rest)
-    .catch((err) => {
-      res.status(500).json({
-        error: `Could not update Expense ${err}`,
-        expenses: null,
-      });
-    });
+  try {
+    const updateExpenseRespose = await new ExpenseService().updateExpense(id, rest);
 
-  res.status(200).json({
-    error: null,
-    expenses: {
-      updated: updateExpenseRespose[0],
-    },
-  });
+    res.status(200).json({
+      error: null,
+      expenses: {
+        updated: updateExpenseRespose[0],
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: `Could not update Expense ${err}`,
+      expenses: null,
+    });
+  }
 };
 
 export const deleteExpense = async (req: Request, res: Response) => {
   const { id } = req.body;
-  const deleteExpenseResponse = await new ExpenseService().deleteExpense(id)
-    .catch((err) => {
-      res.status(500).json({
-        error: `Could not delete expense ${err}`,
-        expenses: null,
-      });
-    });
+  try {
+    const deleteExpenseResponse = await new ExpenseService().deleteExpense(id);
 
-  res.status(200).json({
-    error: null,
-    expenses: {
-      deleted: deleteExpenseResponse,
-    },
-  });
+    res.status(200).json({
+      error: null,
+      expenses: {
+        deleted: deleteExpenseResponse,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: `Could not delete expense ${err}`,
+      expenses: null,
+    });
+  }
 };
--- src/incomes/incomes.controller.ts
+++ src/incomes/incomes.controller.ts
@@ -1,88 +1,91 @@
 
-import { ICreateIncomeOutput, IGetIncomesOutput } from "./incomes.ds";
-
 import {Request, Response} from "express";
 import { IncomeService } from "./incomes.model";
 
 export const getAllIncomes = async (req: Request, res: Response) => {
-  const { incomes } = await new IncomeService().getAllIncomes()
-    .catch((err) => {
-      res.status(500).json({
-        error: `Could not retrieve all incomes ${err}`,
-        incomes: null,
-      });
-    });
+  try {
+    const { incomes } = await new IncomeService().getAllIncomes();
 
-  res.status(200).json({
-    error: null,
-    incomes,
-  });
+    res.status(200).json({
+      error: null,
+      incomes,
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: `Could not retrieve all incomes ${err}`,
+      incomes: null,
+    });
+  }
 };
 
 export const getMonthlyIncome = async (req: Request, res: Response) => {
   const { month, year } = req.params;
-  const { incomes } = await new IncomeService().getMonthlyIncomes(month, year)
-    .catch((err) => {
-      res.status(500).json({
-        error: `Could not retrieve monthly incomes ${err}`,
-        incomes: null,
-      });
-    });
+  try {
+    const { incomes } = await new IncomeService().getMonthlyIncomes(month, year);
 
-  res.status(200).json({
-    error: null,
-    incomes,
-  });
+    res.status(200).json({
+      error: null,
+      incomes,
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: `Could not retrieve monthly incomes ${err}`,
+      incomes: null,
+    });
+  }
 };
 
 export const createIncome = async (req: Request, res: Response) => {
   const createIncomeReq = req.body;
-  const createIncomeResponse = await new IncomeService().createIncome(createIncomeReq)
-    .catch((err) => {
-      res.status(500).json({
-        error: `Could not create income ${err}`,
-        income: null,
-      });
-    });
+  try {
+    const createIncomeResponse = await new IncomeService().createIncome(createIncomeReq);
 
-  res.status(200).json({
-    error: null,
-    income: createIncomeResponse,
-  });
+    res.status(200).json({
+      error: null,
+      income: createIncomeResponse,
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: `Could not create income ${err}`,
+      income: null,
+    });
+  }
 };
 
 export const updateIncome = async (req: Request, res: Response) => {
   const { id, ...rest } = req.body;
-  const updateIncomeRespose = await new IncomeService().updateIncome(id, rest)
-    .catch((err) => {
-      res.status(500).json({
-        error: `Could not update income ${err}`,
-        income: null,
-      });
-    });
+  try {
+    const updateIncomeRespose = await new IncomeService().updateIncome(id, rest);
 
-  res.status(200).json({
-    error: null,
-    income: {
-      updated: updateIncomeRespose[0],
-    },
-  });
+    res.status(200).json({
+      error: null,
+      income: {
+        updated: updateIncomeRespose[0],
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: `Could not update income ${err}`,
+      income: null,
+    });
+  }
 };
 
 export const deleteIncome = async (req: Request, res: Response) => {
   const { id } = req.body;
-  const deleteIncomeResponse = await new IncomeService().deleteIncome(id)
-    .catch((err) => {
-      res.status(500).json({
-        error: `Could not delete income ${err}`,
-        income: null,
-      });
-    });
+  try {
+    const deleteIncomeResponse = await new IncomeService().deleteIncome(id);
 
-  res.status(200).json({
-    error: null,
-    income: {
-      deleted: deleteIncomeResponse,
-    },
-  });
+    res.status(200).json({
+      error: null,
+      income: {
+        deleted: deleteIncomeResponse,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: `Could not delete income ${err}`,
+      income: null,
+    });
+  }
 };
