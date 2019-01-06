3bcb7c46fea202c8d5509e3bd468cf1b086fad11
Alexandre Resende <aar6947@gmail.com>
2019-01-06 16:04:47 -0200

    chore: Create middleware to handle allowed categories

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import * as helmet from "helmet";
 
 import { ExpenseRoutes } from "./expenses/expenses";
 import { IncomeRoutes } from "./incomes/incomes";
+import CategoryMiddleware from "./middlewares/categories.middleware";
 
 class App {
   public app: express.Application;
@@ -21,6 +22,7 @@ class App {
       .use(helmet())
       .use(bodyParser.urlencoded({ extended: true }))
       .use(bodyParser.json())
+      .use(CategoryMiddleware)
       .use(this.incomeRoutes.getIncomeRoutes())
       .use(this.expenseRoutes.getExpenseRoutes());
   }
