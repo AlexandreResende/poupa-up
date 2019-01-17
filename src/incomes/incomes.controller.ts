28e7175fa6a31817249f953afbf933f3c13933ce
Alexandre Resende <aar6947@gmail.com>
2019-01-16 23:16:28 -0200

    chore: Remove unnecessary logs

--- src/incomes/incomes.controller.ts
+++ src/incomes/incomes.controller.ts
@@ -5,14 +5,13 @@ import { IncomeService } from "./incomes.model";
 export const getAllIncomes = async (req: Request, res: Response) => {
   try {
     const { incomes } = await new IncomeService().getAllIncomes();
-    console.log('AKIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII');
-    console.log(incomes);
+
     res.status(200).json({
       error: null,
       incomes,
     });
   } catch (err) {
-    console.log(err);
+
     res.status(500).json({
       error: `Could not retrieve all incomes ${err}`,
       incomes: null,
