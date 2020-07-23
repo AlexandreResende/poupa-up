a2f9cbc40be1c66f705c9a3e88ea85a574950f2f
Alexandre Resende <alexandre.resende@gmail.com>
2020-07-22 21:48:01 -0300

    chore: Create http response class and enum

---
+++ src/application/http-response-handler.ts
@@ -0,0 +1,14 @@
+import { Response } from "express";
+import { StatusCodes } from "../http-status-code";
+
+export class HttpResponseHandler {
+  private readonly response: Response;
+
+  constructor(response: Response) {
+    this.response = response;
+  }
+
+  sendSuccess(data: object) {
+    return this.response.status(StatusCodes.OK).send(data);
+  }
+}
---
+++ src/http-status-code.ts
@@ -0,0 +1,6 @@
+import { STATUS_CODES } from "http";
+export enum StatusCodes {
+  OK = 200,
+  NOT_FOUND = 404,
+  INTERNAL_SERVER_ERROR = 500
+}
