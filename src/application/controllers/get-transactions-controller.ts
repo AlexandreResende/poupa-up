0067bef56214eb398b1854690b6fda25387a797c
Alexandre Resende <alexandre.resende@gmail.com>
2020-07-28 18:09:32 -0300

    wip: Made simples insert on transaction table on controller

--- src/application/controllers/get-transactions-controller.ts
+++ src/application/controllers/get-transactions-controller.ts
@@ -1,7 +1,18 @@
 import { Request, Response } from "express";
+import TransactionRepository from "../repositories/transaction-repository"
 
 export class GetTransactionsController {
-  handleRequest(_: Request, res: Response): object {
+  async handleRequest(_: Request, res: Response): Promise<object> {
+    const transactionRepository = new TransactionRepository();
+
+    await transactionRepository.create({
+      description: "nada",
+      month: "02",
+      year: "2020",
+      valueSpent: -20.50,
+      category: "lanche"
+    });
+
     return res.status(200).send({ data: "Done..." });
   }
 }
