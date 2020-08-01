540bf995f58fbded2ebe79d35b653eb69947def7
Alexandre Resende <alexandre.amres@gmail.com>
2020-08-01 20:29:08 -0300

    chore: Create expressHandler for centralizing error handling

---
+++ src/application/express-handler.ts
@@ -0,0 +1,12 @@
+import HttpResponseHandler from "./http-response-handler";
+import { NextFunction, Response, Request } from "express";
+
+export const expressHandler = (controller: any) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await controller(req, res, next);
+    } catch (error) {
+      return HttpResponseHandler.sendInternalError(res, error);
+    }
+  };
+};
