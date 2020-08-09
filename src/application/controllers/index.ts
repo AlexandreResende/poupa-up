313d84afba55509af112acea57fb7751aeb0e774
Alexandre Resende <alexandre.amres@gmail.com>
2020-08-09 14:30:17 -0300

    chore: Add update-transaction to index.js

--- src/application/controllers/index.ts
+++ src/application/controllers/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { expressHandler } from "../express-handler";
 import GetTransactionsController from "./transactions/get-transactions-controller";
 import CreateTransactionController from "./transactions/create-transaction-controller";
+import UpdateTransactionController from "./transactions/update-transaction-controller";
 
 export class Routes {
   public routes: Router;
@@ -15,7 +16,8 @@ export class Routes {
   private configRoutes(): void {
     this.routes
       .get("/transaction", expressHandler(new GetTransactionsController().handleRequest))
-      .post('/transaction', expressHandler(new CreateTransactionController().handleRequest));
+      .post("/transaction", expressHandler(new CreateTransactionController().handleRequest))
+      .put("/transaction", expressHandler(new UpdateTransactionController().handleRequest));
   }
 
   public getRoutes(): Router {
