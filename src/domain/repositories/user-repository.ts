97527ba496257c65d4321623127336475fc4bc91
Alexandre Resende <alexandre.amres@gmail.com>
2020-08-23 14:34:59 -0300

    chore: Create user-repository

---
+++ src/domain/repositories/user-repository.ts
@@ -0,0 +1,30 @@
+import { sequelize } from "../../database-config";
+import User from "../../domain/models/user-model"
+import { Repository } from "sequelize-typescript";
+import { User as UserInterface, UserRepositoryDataInterface } from "../../application/interfaces/user-interface";
+import UserRepositoryInterface from "../../application/interfaces/repository-interfaces/user-repository-interface"
+import UpdateUserInterface from "../../application/interfaces/update-user-interface";
+
+export default class UserRepository implements UserRepositoryInterface{
+  public readonly repository: Repository<User>;
+
+  constructor() {
+    this.repository = sequelize.getRepository(User);
+  }
+
+  async create(userData: UserInterface): Promise<UserRepositoryDataInterface> {
+    return this.repository.create(userData);
+  }
+
+  async update(id: string, updateUserData: UpdateUserInterface): Promise<{ updated: number }> {
+    const [ valuesUpdated ] = await this.repository.update(updateUserData, { where: { id } });
+
+    return { updated: valuesUpdated };
+  }
+
+  async remove(id: string): Promise<{ removed: number}> {
+    const valuesRemoved = await this.repository.destroy({ where: { id } });
+
+    return { removed: valuesRemoved };
+  }
+}
