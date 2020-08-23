3254ab3c77edf67b4a2cc8f969cd5f476e464dc0
Alexandre Resende <alexandre.amres@gmail.com>
2020-08-23 14:34:37 -0300

    chore: Adjust user-repository-interface

--- src/application/interfaces/repository-interfaces/user-repository-interface.ts
+++ src/application/interfaces/repository-interfaces/user-repository-interface.ts
@@ -1,8 +1,8 @@
-import { User } from "../user-interface";
+import { User as UserInterface } from "../user-interface";
 import UpdateUserInterface from "../update-user-interface";
 
 export default interface UserRepositoryInterface {
-  create(user: User): Promise<User>;
+  create(user: UserInterface): Promise<UserInterface>;
   update(id: string, updatedUser: UpdateUserInterface): Promise<{ updated: number }>;
   remove(id: string): Promise<{ removed: number }>;
 }
