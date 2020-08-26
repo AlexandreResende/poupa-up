d46cb2010a7464e3eb48b497843a1cd00239250a
Alexandre Resende <alexandre.amres@gmail.com>
2020-08-26 20:38:37 -0300

    fix: Adjust cryptography-service

--- src/application/services/cryptography-service.ts
+++ src/application/services/cryptography-service.ts
@@ -1,24 +1,21 @@
 import { createCipher, createDecipher, Cipher } from "crypto";
 
 export default class CryptographyService {
-  private readonly algorithm: string = process.env.algorithm ?? "";
-
-  constructor() {
-
-  }
+  private readonly algorithm: string = process.env.CRYPTO_ALGORITHM ?? "";
+  private readonly password:string = process.env.CRYPTO_PASSWORD ?? "";
 
   public encrypt(word: string): string {
-    let mykey: Cipher = createCipher(this.algorithm, word);
-    let mystr = mykey.update('abc', 'utf8', 'hex')
-    mystr += mykey.final('hex');
+    let mykey: Cipher = createCipher(this.algorithm, this.password);
+    let mystr = mykey.update(word, "utf8", "hex")
+    mystr += mykey.final("hex");
 
     return mystr;
   }
 
   public decrypt(encryptedWord: string): string {
-    let mykey = createDecipher(this.algorithm, encryptedWord);
-    let mystr = mykey.update('34feb914c099df25794bf9ccb85bea72', 'hex', 'utf8')
-    mystr += mykey.final('utf8');
+    let mykey = createDecipher(this.algorithm, this.password);
+    let mystr = mykey.update(encryptedWord, "hex", "utf8");
+    mystr += mykey.final("utf8");
 
     return mystr;
   }
