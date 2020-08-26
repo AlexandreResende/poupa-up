6cf296c4a564faefa9a46baf71d436121acc1868
Alexandre Resende <alexandre.amres@gmail.com>
2020-08-25 21:20:06 -0300

    chore: Improve crypto-service

--- src/application/services/cryptography-service.ts
+++ src/application/services/cryptography-service.ts
@@ -1,10 +1,14 @@
 import { createCipher, createDecipher, Cipher } from "crypto";
 
 export default class CryptographyService {
-  constructor() {}
+  private readonly algorithm: string = process.env.algorithm ?? "";
+
+  constructor() {
+
+  }
 
   public encrypt(word: string): string {
-    let mykey: Cipher = createCipher('aes-128-cbc', word);
+    let mykey: Cipher = createCipher(this.algorithm, word);
     let mystr = mykey.update('abc', 'utf8', 'hex')
     mystr += mykey.final('hex');
 
@@ -12,7 +16,7 @@ export default class CryptographyService {
   }
 
   public decrypt(encryptedWord: string): string {
-    let mykey = createDecipher('aes-128-cbc', encryptedWord);
+    let mykey = createDecipher(this.algorithm, encryptedWord);
     let mystr = mykey.update('34feb914c099df25794bf9ccb85bea72', 'hex', 'utf8')
     mystr += mykey.final('utf8');
 
