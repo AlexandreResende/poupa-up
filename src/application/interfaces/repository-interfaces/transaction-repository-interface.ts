b50b586616613072a21c80f106f603f67b9d9570
Alexandre Resende <alexandre.amres@gmail.com>
2020-08-29 16:05:10 -0300

    chore: Adjust transaction interface

--- src/application/interfaces/repository-interfaces/transaction-repository-interface.ts
+++ src/application/interfaces/repository-interfaces/transaction-repository-interface.ts
@@ -1,8 +1,9 @@
-import { Transaction } from "../transaction-interface";
+import { Transaction as TransactionInterface } from "../transaction-interface";
 import UpdateTransactionInterface from "../update-transaction-interface";
+import Transaction from "../../../domain/entities/transaction-entity";
 
 export default interface TransactionRepositoryInterface {
-  create(transaction: Transaction): Promise<Transaction>;
+  create(transaction: TransactionInterface): Promise<Transaction>;
   findAll(): Promise<Transaction[]>;
   update(id: string, updatedTransaction: UpdateTransactionInterface): Promise<{ updated: number }>;
   remove(id: string): Promise<{ removed: number }>;
