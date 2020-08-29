940b05caff57cc2e018450cbdce5fd7e640bb87c
Alexandre Resende <alexandre.amres@gmail.com>
2020-08-29 15:12:03 -0300

    chore: Create transaction mapper

---
+++ src/application/mapper/transaction-mapper.ts
@@ -0,0 +1,23 @@
+import Transaction from "../../domain/entities/transaction-entity";
+import {
+  Transaction as TransactionInterface,
+  TransactionRepositoryDataInterface as TransactionRepositoryInterface
+} from "../../application/interfaces/transaction-interface";
+
+interface TransactionToDatabase {
+  value?: number;
+  description?: string;
+  category?: string;
+  month?: string;
+  year?: string;
+}
+
+export const fromDatabase = (attributes: TransactionRepositoryInterface) => new Transaction(attributes);
+
+export const toDatabase = (attributes: TransactionToDatabase) => ({
+  value: attributes.value ?? undefined,
+  description: attributes.description ?? undefined,
+  category: attributes.category ?? undefined,
+  month: attributes.month ?? undefined,
+  year: attributes.year ?? undefined,
+});
