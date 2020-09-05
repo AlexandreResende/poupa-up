3dd6108566a4e001654c1d3088ca6396f78c643a
Alexandre Resende <alexandre.amres@gmail.com>
2020-09-05 08:02:03 -0300

    chore: Update file using module alias

--- src/domain/commands/transactions/import-transactions-csv-command.ts
+++ src/domain/commands/transactions/import-transactions-csv-command.ts
@@ -1,7 +1,7 @@
-import { BaseCommandInterface } from "../base-command-interface";
+import { BaseCommandInterface } from "@src/domain/commands/base-command-interface";
 import { EventEmitter } from "events";
-import TransactionRepositoryInterface from "../../../application/interfaces/repository-interfaces/transaction-repository-interface";
-import { Transaction as TransactionInterface } from "../../../application/interfaces/transaction-interface";
+import TransactionRepositoryInterface from "@src/application/interfaces/repository-interfaces/transaction-repository-interface";
+import { Transaction as TransactionInterface } from "@src/application/interfaces/transaction-interface";
 
 export class ImportTransactionsCSVCommand implements BaseCommandInterface {
   private readonly events: EventEmitter;
