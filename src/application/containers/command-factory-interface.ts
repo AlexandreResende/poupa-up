1adc6a10b714e20e595e7d3cc4c1c04ca351bc53
Alexandre Resende <alexandre.amres@gmail.com>
2020-09-11 21:14:03 -0300

    chore: Update file with module alias

--- src/application/containers/command-factory-interface.ts
+++ src/application/containers/command-factory-interface.ts
@@ -1,5 +1,5 @@
 import { EventEmitter } from "events";
-import { BaseCommandInterface } from "../../domain/commands/base-command-interface";
+import { BaseCommandInterface } from "@src/domain/commands/base-command-interface";
 
 export default interface CommandFactoryInterface {
   create(events: EventEmitter): BaseCommandInterface;
