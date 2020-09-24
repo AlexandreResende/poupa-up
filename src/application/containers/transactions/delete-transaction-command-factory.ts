2b0ce122dd131839512cbddba1726e2cd1fa4900
Alexandre Resende <alexandre.amres@gmail.com>
2020-09-23 21:01:32 -0300

    chore: Update file with module alias

--- src/application/containers/transactions/delete-transaction-command-factory.ts
+++ src/application/containers/transactions/delete-transaction-command-factory.ts
@@ -1,6 +1,6 @@
 import { EventEmitter } from "events";
-import DeleteTransactionCommand from "../../../domain/commands/transactions/delete-transaction-command";
-import TransactionRepository from "../../../domain/repositories/transaction-repository";
+import DeleteTransactionCommand from "@src/domain/commands/transactions/delete-transaction-command";
+import TransactionRepository from "@src/domain/repositories/transaction-repository";
 
 export default class DeleteTransactionCommandFactory {
   create(events: EventEmitter): DeleteTransactionCommand {
