78257a7f095417bf18f7dc9d7b76704f7153760f
Alexandre Resende <alexandre.amres@gmail.com>
2021-03-30 08:07:40 -0300

    test: Create test for command-factory

---
+++ test/unit/command-factory/transactions/get-transacions-command-factory.spec.ts
@@ -0,0 +1,19 @@
+import Container from "@src/application/containers/container";
+import { GetTransactionsCommand } from "@src/domain/commands/transactions/get-transactions-command"
+import { EventEmitter } from "events";
+
+const { expect } = require("chai");
+
+describe.only("GetTransactionsCommandFactory", () => {
+  it("return an instance of GetTransactionsCommand when called", () => {
+    // given
+    const commandName = "getTransactionsCommand";
+    const events = new EventEmitter();
+
+    // when
+    const getTransactionsCommand = Container.resolve(commandName, events);
+
+    // then
+    expect(getTransactionsCommand).to.be.instanceof(GetTransactionsCommand);
+  });
+});
