cf5b0825c05677ff8166360c24b352ae2152cc75
Alexandre Resende <alexandre.amres@gmail.com>
2021-04-07 22:50:08 -0300

    fix: Adjust validator to return legible error to clients

--- src/application/validator.ts
+++ src/application/validator.ts
@@ -2,21 +2,31 @@ import { validate } from "validate-typescript";
 
 interface ValidatorReturn {
   isValid: boolean;
-  error?: Error
+  error?: string[]
 }
 
 export class Validator {
   public static validate(schema: object, input: object): ValidatorReturn {
     try {
-      validate(schema, input);
+      const result = validate(schema, input);
 
       return {
         isValid: true
       };
-    } catch (error) {
+    } catch (err) {
+      const errorResult: string[] = [];
+
+      err.child_errors.forEach((error: { property: string, child_error: { details: string } }) => {
+        const fieldName: string = error.property;
+        const errorMessage: string = error.child_error.details;
+
+        const validationErrorMessage = `${fieldName.substr(1)} ${errorMessage}`;
+
+        errorResult.push(validationErrorMessage);
+      });
       return {
         isValid: false,
-        error
+        error: errorResult
       };
     }
   }
